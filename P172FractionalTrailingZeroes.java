e060da58253fa4bf43f8611fc58630ae2d04a45b
ysonggit <ysonggit@users.noreply.github.com>
2015-04-27 23:33:12 -0400

    Update P172FractionalTrailingZeroes.java

--- P172FractionalTrailingZeroes.java
+++ P172FractionalTrailingZeroes.java
@@ -1,1 +1,12 @@
-
+public class Solution {
+    // find out how many 5s 
+    // 20! -> 4 trailing zeroes
+    // be careful of overflow
+    public int trailingZeroes(int n) {
+        int count =0;
+        for(long i=5; i<=n; i=i*5){
+            count += n/i;
+        }
+        return count;
+    }
+}
