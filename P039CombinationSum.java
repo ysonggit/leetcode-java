21de85b4feed6a783e03e4dc222a4cf3e9bcad60
ysonggit <ysonggit@users.noreply.github.com>
2015-05-21 15:35:33 -0400

    Update P039CombinationSum.java

--- P039CombinationSum.java
+++ P039CombinationSum.java
@@ -1,20 +1,16 @@
 public class Solution {
-    public void dfs(List<List<Integer>> res, int[] candidates, List<Integer> sol, int cur_idx, int target){
-        int n = candidates.length;
-        if(target<0 ) return;
-        if(target==0){
+    public void dfs(List<List<Integer>> res, int[] candidates, ArrayList<Integer> sol, int cur_idx, int cur_sum, int target){
+        if(cur_sum==target){
             List<Integer> path = new ArrayList<Integer>(sol);
             res.add(path);
-            return ;
+            return;
         }
-        for(int i=cur_idx; i<n; i++){
-            int cur = candidates[i];
-            if(i>cur_idx && cur==candidates[i-1]) i++;
-            if(target >= cur){ //mistake : don't use > 
-                sol.add(cur);
-                dfs(res, candidates, sol, i, target-cur);
-                sol.remove(sol.size()-1);
-            }
+        if(cur_idx == candidates.length) return;
+        if(cur_sum>target) return;
+        for(int i=cur_idx; i<candidates.length; i++){
+            sol.add(candidates[i]);
+            dfs(res, candidates, sol, i, cur_sum+candidates[i], target);
+            sol.remove(sol.size()-1);
         }
     }
     
@@ -22,9 +18,9 @@ public class Solution {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         int n = candidates.length;
         if(n==0) return res;
-        Arrays.sort(candidates);  // mistake: output format: non-decreasing order
-        List<Integer> sol = new ArrayList<Integer>();
-        dfs(res, candidates, sol, 0, target);
+        ArrayList<Integer> sol = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        dfs(res, candidates, sol, 0, 0, target);
         return res;
     }
 }
