0351f7b4b2676e081a9fefd7a8053e1462876da1
ysonggit <ysonggit@users.noreply.github.com>
2015-05-25 13:07:06 -0400

    Create P217ContainsDuplicate.java

---
+++ P217ContainsDuplicate.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // O(nlogn)
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        if(n<2) return false;
+        Arrays.sort(nums);
+        for(int i=1; i<n; i++){
+            if(nums[i-1]== nums[i]) return true;
+        }
+        return false;
+    }
+}
