b11d1f030847432b6c0906e65dc664a6f481ea46
ysonggit <ysonggit@users.noreply.github.com>
2015-05-26 00:13:29 -0400

    Update P127WordLadder.java

--- P127WordLadder.java
+++ P127WordLadder.java
@@ -1,40 +1,46 @@
 public class Solution {
-    // save extra space, using stringbuffer to make a new one is too expensive 
-    public String replaceWithChar(String s, char c, int i){
-        char [] chars = s.toCharArray();
-        chars[i] = c;
-        return new String(chars);
+    public ArrayList<String> getOneEditWords(String word){
+        ArrayList<String> neighbors = new ArrayList<String>();
+        char wd[] = word.toCharArray();
+        for(int i=0; i<wd.length; i++){
+            char origin = wd[i];
+            for(char c = 'a'; c<='z'; c++){
+                if(wd[i] != c) { 
+                    wd[i] = c;
+                    neighbors.add(new String(wd));
+                    wd[i] = origin;
+                }
+            }
+        }
+        return neighbors;
     }
     public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
         if(wordDict.size()==0) return 0;
         Queue<String> Q = new LinkedList<String>();
+        Q.offer(beginWord);
+        HashSet<String> visited = new HashSet<String>();
+        visited.add(beginWord);
         int len = 1;
         int curr_level = 1, next_level=0;
-        Q.offer(beginWord);
-        wordDict.remove(beginWord); 
         while(!Q.isEmpty()){
             String cur = Q.poll();
             curr_level--;
-            for(int i=0; i<cur.length(); i++){
-                char c = cur.charAt(i);
-                for(char r = 'a'; r <='z'; r++){
-                    if(c != r){
-                        String word = replaceWithChar(cur, r, i); 
-                        if(word.toString().equals(endWord)) return len+1;
-                        if(wordDict.contains(word)){
-                            Q.offer(word);
-                            wordDict.remove(word);
-                            next_level++;
-                        }
+            for(String next : getOneEditWords(cur)){
+                if(next.equals(endWord)){ return len+1;}
+                if(wordDict.contains(next)) {
+                    if(!visited.contains(next)){
+                        Q.offer(next);
+                        visited.add(next);
+                        next_level++;
                     }
                 }
             }
-            if(curr_level == 0){
+            if(curr_level==0){
                 curr_level = next_level;
                 next_level = 0;
                 len++;
             }
         }
-        return 0; // mistake : return 0 not len "hot" "dog" ["hot" "dog"]
+        return 0;
     }
 }
