36fc4e878a93b33c60ed09f0317d7e26f7995583
ysonggit <ysonggit@users.noreply.github.com>
2015-06-04 01:36:58 -0400

    Create P032LongestValidParentheses.java

---
+++ P032LongestValidParentheses.java
@@ -0,0 +1,29 @@
+public class Solution {
+    // (())(())
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        if(n==0) return 0;
+        int max_len = 0; 
+        int last_right_paren_pos = -1; // trick
+        Stack<Integer> left_parens_pos = new Stack<Integer>();
+        for(int i=0; i<n; i++){
+            char cur = s.charAt(i);
+            if(cur == '('){
+                left_parens_pos.push(i);
+            }else{
+                // ')' determine the length of valid parentheses pair 
+                if(left_parens_pos.empty()){
+                    last_right_paren_pos = i;
+                }else{
+                    left_parens_pos.pop();
+                    if(left_parens_pos.empty()){
+                        max_len = Math.max(max_len, i-last_right_paren_pos);
+                    }else{
+                        max_len = Math.max(max_len, i-left_parens_pos.peek());
+                    }
+                }
+            }
+        }
+        return max_len;
+    }
+}
