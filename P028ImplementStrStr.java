8b948db58863fdab39ace9f6785cade171171d9e
ysonggit <ysonggit@users.noreply.github.com>
2015-06-10 15:57:12 -0400

    Update P028ImplementStrStr.java

--- P028ImplementStrStr.java
+++ P028ImplementStrStr.java
@@ -14,3 +14,52 @@ public class Solution {
         return -1;
     }
 }
+
+public class Solution {
+    /* KMP
+       create failure function, table T[i] means
+       the steps of backtracking if needle[i] != haystack[m+i],  
+       restart search from needle [T[i]], haystack[m+i-T[i]]
+    */
+    public void computeFailureFunction(int []T, String W){
+        T[0] = -1;
+        T[1] = 0;
+        int idx = 0, pos = 2;
+        while(pos<W.length()){
+            if(W.charAt(idx) == W.charAt(pos-1)){
+                idx++;
+                T[pos] = idx;
+                pos++;
+            }else if(idx > 0){
+                // restore idx
+                idx = T[idx];
+            }else{
+                T[pos]=0;
+                pos++;
+            }
+        }
+    }
+    public int strStr(String haystack, String needle) {
+        int n = needle.length();
+        if(n==0 ) return 0;
+        if(n==1) return haystack.indexOf(needle.charAt(0));
+        int []T = new int[n];
+        computeFailureFunction(T, needle);
+        int m=0, i=0;
+        while(m+i<haystack.length()){
+            if(haystack.charAt(m+i) == needle.charAt(i)){
+                i++;
+                if(i==n) return m;
+            }else{
+                if(T[i]>-1){
+                    m=m+i-T[i];
+                    i=T[i];
+                }else{
+                    m++;
+                    i=0;
+                }
+            }
+        }
+        return -1;
+    }
+}
