7f11db6c20b9e28d0bedb3cd32ccab09b2554e02
ysonggit <ysonggit@users.noreply.github.com>
2015-06-10 23:55:44 -0400

    Create P214ShortestPalindrome.java

---
+++ P214ShortestPalindrome.java
@@ -0,0 +1,44 @@
+public class Solution {
+    // KMP compute prefix table T[0] means the length of the longest
+    // prefix of s that is also the suffix of s[0...j]
+    // see strstr
+    public void computePrefixTable(int[] T, String s){
+        T[0] =0;
+        int i=0, j=1;
+        while(j<s.length()){
+            if(s.charAt(i) == s.charAt(j)){
+                T[j] = i+1;
+                i++;
+                j++;
+            }else if(i>0){
+                i=T[i-1]; // restore i
+            }else{
+                T[j] = 0;
+                j++;
+            }
+        }
+    }
+    /* http://blog.csdn.net/pointbreak1/article/details/45931551
+       1. reverse string -> rev
+       2. concat p=s+rev
+       3. find prefix table T of p using KMP
+       4. return rev.substring(0, N-T[2N-1])+s  (N is length of s)
+       Example s="abbacd"
+       rev="dcabba" => 
+       p : a b b a c d d c a b b a
+       T : 0 0 0 1 0 0 0 0 1 2 3 4
+                      |---|
+                        |___ substring we need to prepend to original one
+    */
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+        if(n<2) return s;
+        String rev = new StringBuilder(s).reverse().toString();
+        String p = s+rev;
+        int [] T = new int [2*n];
+        computePrefixTable(T, p);
+        // length of the longest prefix of p that is also the suffix of p
+        int len_longest_prefix = T[2*n-1];
+        return rev.substring(0, n-len_longest_prefix)+s;
+    }
+}
