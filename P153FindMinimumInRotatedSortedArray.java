84a29ef27502107822ca08970a38d3cd0d811d49
ysonggit <ysonggit@users.noreply.github.com>
2015-09-11 00:54:28 -0700

    Update P153FindMinimumInRotatedSortedArray.java

--- P153FindMinimumInRotatedSortedArray.java
+++ P153FindMinimumInRotatedSortedArray.java
@@ -4,19 +4,13 @@ public class Solution {
         if(n==0) return 0;
         int front=0, back=n-1;
         while(front<back){
-            int mid = (front+back)/2;
-            if(mid<back&& nums[mid]> nums[mid+1]){
-                return nums[mid+1];
-            }else if(mid>front  && nums[mid] < nums[mid-1]){
-                return nums[mid];
-            }else if(nums[mid]>nums[back]) {
-                // [4 5 6 7 0 1 2]
-                front=mid+1;
+            int mid = front+(back-front)/2;
+            if(nums[mid] > nums[back]){
+                front = mid+1;
             }else{
-                // [7 0 1 2 3 4]
-                back = mid-1;
+                back = mid;
             }
         }
-        return nums[0];
+        return nums[back];
     }
 }
