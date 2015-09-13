ad4c287732eca5784730b1db04cb7339850dcaa0
ysonggit <ysonggit@users.noreply.github.com>
2015-09-13 01:07:49 -0700

    Update P053MaximumSubarray.java

--- P053MaximumSubarray.java
+++ P053MaximumSubarray.java
@@ -1,22 +1,20 @@
 public class Solution {
-    /*
-        track two variables: max_ending_here (local), max_so_far (global)
-                       [−2,1,−3, 4,−1,2,1,−5,4]
-       max_ending_here   0 1  0  4  3 5 6 1  5
-       max_so_far       -2 1  1  4  4 5 6 6  6
-       
-                       [-2, -1]   [-2 1]
-                        -2  -1     -2 1
-                        -2  -1     -2 1
-    */
+    // DP solution:
+    // D[i]  the maximum subarray sum of first i numbers
+    // D[i] = max(A[i], A[i]+D[i-1])
+    // D[0] = 0
+    // return max(D[i]) for i in [0,n-1]
     public int maxSubArray(int[] nums) {
         int n = nums.length;
         if(n==0) return 0;
-        int max_so_far = nums[0], max_ending_here = nums[0];// mistake max_ending_here=nums[0]
-        for(int i=1; i<n; i++){
-            max_ending_here = Math.max(nums[i], nums[i]+max_ending_here);
-            max_so_far = Math.max(max_so_far, max_ending_here);
+        int D[] = new int[n+1];
+        int max_idx = 1;
+        for(int i=1; i<=n; i++){
+            D[i]= Math.max(nums[i-1], nums[i-1]+D[i-1]);
+            if(D[i] > D[max_idx]){
+                max_idx = i;
+            }
         }
-        return max_so_far;
+        return D[max_idx];
     }
 }
