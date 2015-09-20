4aedde68d74b5e3405658873d141d2a14159ecd9
ysonggit <ysonggit@users.noreply.github.com>
2015-09-19 22:48:28 -0700

    Update P070ClimbStairs.java

--- P070ClimbStairs.java
+++ P070ClimbStairs.java
@@ -1,10 +1,11 @@
 public class Solution {
-    HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
     public int climbStairs(int n) {
-        if(n<=2) return n;
-        if(cache.containsKey(n)) return cache.get(n);
-        int num_ways = climbStairs(n-1)+climbStairs(n-2);
-        cache.put(n, num_ways);
-        return num_ways;
+        if(n<0) return 0;
+        int D[] = new int [n+1];
+        D[0] = D[1] = 1;
+        for(int i=2; i<=n; i++) {
+            D[i] = D[i-1]+D[i-2];
+        }
+        return D[n];
     }
 }
