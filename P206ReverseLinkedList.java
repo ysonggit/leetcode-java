df9bcc446ca485765c09a0ccd55f70e574be9d84
ysonggit <ysonggit@users.noreply.github.com>
2015-09-22 23:30:12 -0700

    Update P206ReverseLinkedList.java

--- P206ReverseLinkedList.java
+++ P206ReverseLinkedList.java
@@ -6,7 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-public class Solution {
+public class Solution1 {
     public ListNode reverseList(ListNode head) {
         if(head==null) return null;
         ListNode cur = head;
@@ -19,5 +19,25 @@ public class Solution {
             cur = nexcp;
         }
         return pre;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution2 {
+    // 1->2
+    public ListNode reverseList(ListNode head) {
+        if(head == null|| head.next == null) return head;
+        ListNode second = head.next;
+        head.next = null;
+        ListNode rev_rest = reverseList(second);
+        second.next = head;
+        return rev_rest;
     }
 }
