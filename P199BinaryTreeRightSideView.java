c25e8823bef94804e5361ce735a0147316afd82c
ysonggit <ysonggit@users.noreply.github.com>
2015-09-27 16:27:26 -0700

    Update P199BinaryTreeRightSideView.java

--- P199BinaryTreeRightSideView.java
+++ P199BinaryTreeRightSideView.java
@@ -8,34 +8,17 @@
  * }
  */
 public class Solution {
-    // level order traversal, save the right most value
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> vals = new ArrayList<Integer>();
-        if(root==null) return vals;
-        Queue<TreeNode> Q = new LinkedList<TreeNode>();
-        Q.offer(root);
-        int curr_level = 1;
-        int next_level = 0;
-        ArrayList<Integer> cur_level_vals = new ArrayList<Integer>();
-        while(!Q.isEmpty()){
-            TreeNode cur = Q.poll();
-            curr_level--;
-            cur_level_vals.add(cur.val);
-            if(cur.left != null){
-                Q.offer(cur.left);
-                next_level++;
-            }
-            if(cur.right !=null){
-                Q.offer(cur.right);
-                next_level++;
-            }
-            if(curr_level==0){
-                curr_level = next_level;
-                next_level = 0;
-                vals.add(cur_level_vals.get(cur_level_vals.size()-1));
-                cur_level_vals.clear();
-            }
+    void dfs(List<Integer> res, TreeNode cur, int level){
+        if(cur==null) return;
+        if(level== res.size()) {
+            res.add(cur.val);
         }
-        return vals;
+        dfs(res, cur.right, level+1);
+        dfs(res, cur.left, level+1);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        dfs(res, root, 0);
+        return res;
     }
 }
