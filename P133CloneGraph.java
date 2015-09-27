d9c706666ec5be58b4a39e270b3934c58c513848
ysonggit <ysonggit@users.noreply.github.com>
2015-09-26 23:46:07 -0700

    Update P133CloneGraph.java

--- P133CloneGraph.java
+++ P133CloneGraph.java
@@ -5,8 +5,9 @@
  *     List<UndirectedGraphNode> neighbors;
  *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
  * };
+ * BFS
  */
-public class Solution {
+public class Solution1 {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if(node == null) return null;
         HashMap<Integer, UndirectedGraphNode> cloned = new HashMap<Integer, UndirectedGraphNode>();
@@ -19,6 +20,35 @@ public class Solution {
                 if(!cloned.containsKey(neighbor.label)){
                     cloned.put(neighbor.label, new UndirectedGraphNode(neighbor.label));
                     Q.offer(neighbor);
+                }
+                cloned.get(cur.label).neighbors.add(cloned.get(neighbor.label));
+            }
+        }
+        return cloned.get(node.label);
+    }
+}
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ * DFS
+ */
+public class Solution2 {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if(node == null) return null;
+        HashMap<Integer, UndirectedGraphNode> cloned = new HashMap<Integer, UndirectedGraphNode>();
+        Stack<UndirectedGraphNode> Q = new Stack<UndirectedGraphNode>();
+        Q.push(node);
+        cloned.put(node.label, new UndirectedGraphNode(node.label));
+        while(!Q.empty()){
+            UndirectedGraphNode cur = Q.pop();
+            for(UndirectedGraphNode neighbor : cur.neighbors){
+                if(!cloned.containsKey(neighbor.label)){
+                    cloned.put(neighbor.label, new UndirectedGraphNode(neighbor.label));
+                    Q.push(neighbor);
                 }
                 cloned.get(cur.label).neighbors.add(cloned.get(neighbor.label));
             }
