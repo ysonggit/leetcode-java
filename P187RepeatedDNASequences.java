fa0f6e3ffe8377d0a6b85a55beb845ec6b7f1c54
ysonggit <ysonggit@users.noreply.github.com>
2015-10-13 17:40:36 -0400

    Update P187RepeatedDNASequences.java

--- P187RepeatedDNASequences.java
+++ P187RepeatedDNASequences.java
@@ -1,49 +1,38 @@
 public class Solution {
-    // given a 10 letter-long sequence, encode it to an integer (32bit)
-    // A->00 
-    // C->01
-    // G->10
-    // T->11
+    // use an integer to encode a 10-character string:
+    // A: 00 | C: 01 | G: 10 | T: 11
+    HashMap<Character, Integer> table = new HashMap<Character, Integer>();
     public int encode(String seq){
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        map.put('A', 0);
-        map.put('C', 1);
-        map.put('G', 2);
-        map.put('T', 3);
-        int num = 0;
+        int key = 0;
         for(int i=0; i<seq.length(); i++){
-            char c = seq.charAt(i);
-            num = num << 2;
-            int mask = map.get(c);
-            num = num | mask; // mistake : use | not & !
+            int mask= table.get(seq.charAt(i));
+            key |= mask;
+            key <<= 2;
         }
-        return num;
-    }
-    public String decode(int num){
-        int mask = 3; // rightmost two bits
-        StringBuilder seq = new StringBuilder();
-        char letters[] = {'A','C','G','T'};
-        for(int i=0; i<10; i++){
-            int b = num & mask; // use & 
-            num = num>>2;
-            seq.insert(0, letters[b]);
-        }
-        return seq.toString();
+        return key;
     }
     public List<String> findRepeatedDnaSequences(String s) {
-        int n = s.length();
         List<String> res = new ArrayList<String>();
-        if(n<=10) return res;
-        HashMap<Integer, Integer> count= new HashMap<Integer, Integer>();
-        for(int i=0; i<n-9; i++){
+        int n = s.length();
+        if(n<10) return res;
+        table.put('A', 0);
+        table.put('C', 1);
+        table.put('G', 2);
+        table.put('T', 3);
+        HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for(int i=0; i<=n-10; i++){
             String seq = s.substring(i, i+10);
-            int num = encode(seq);
-            if(count.containsKey(num)){ count.put(num, count.get(num)+1); }
-            else count.put(num, 1);
-        }
-        for(Map.Entry<Integer, Integer> entry : count.entrySet()){
-            if(entry.getValue()>1){
-                res.add(decode(entry.getKey()));
+            //int key = seq.hashCode(); 
+            //mistake: different strings produce duplicated keys 
+            //if using C++ hash<string>, works well 
+            int key = encode(seq);
+            if(!counts.containsKey(key)){
+                counts.put(key, 1);
+            }else{
+                counts.put(key, counts.get(key)+1);
+                if(counts.get(key)==2){
+                    res.add(seq);
+                } 
             }
         }
         return res;
