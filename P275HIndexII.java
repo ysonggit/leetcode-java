2a04a18c275d389f3b2f390dedd22de7ff0d39ab
ysonggit <ysonggit@users.noreply.github.com>
2015-10-20 18:46:59 -0700

    Update P275HIndexII.java

--- P275HIndexII.java
+++ P275HIndexII.java
@@ -2,19 +2,15 @@ public class Solution {
     // key: every time record min_of(citations[mid], n-mid)
     public int hIndex(int[] citations) {
         int n = citations.length;
-        int front = 0, back = n-1, h = 0;
+        int front = 0, back = n-1;
         while(front<=back){
             int mid = front+(back-front)/2;
-            if(citations[mid] == n-mid){
-                return citations[mid]; // do not return mid!
-            }else if(citations[mid] < n-mid){
-                h = citations[mid];
-                front++; // can not use mid+1
+            if(citations[mid] < n-mid){
+                front = mid+1;
             }else{
-                h = n-mid;
-                back--; 
+                back = mid-1; 
             }
         }
-        return h;
+        return n-front;
     }
 }
