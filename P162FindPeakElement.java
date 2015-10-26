3570096027a5ab491636edb7a6a8e42e0280dbea
ysonggit <ysonggit@users.noreply.github.com>
2015-10-26 00:05:46 -0700

    Update P162FindPeakElement.java

--- P162FindPeakElement.java
+++ P162FindPeakElement.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
         if(n<2) return 0;
@@ -19,5 +19,21 @@ public class Solution {
               }  
         }
         return 0;
+    }
+}
+
+public class Solution2 {
+    public int binarySearch(int[] nums, int low, int high){
+        if(low>high) return -1;
+        if(high-low<=1) return nums[low] > nums[high] ? low : high; // mistake: must consider this case
+        int mid = (high-low)/2 + low;
+        int left = mid>0 ? nums[mid-1] : Integer.MIN_VALUE;
+        int right = mid<nums.length-1 ? nums[mid+1] : Integer.MIN_VALUE;
+        if(nums[mid] > left && nums[mid]> right) return mid;
+        if(nums[mid] < right) return binarySearch(nums, mid+1, high);
+        return binarySearch(nums, low, mid-1);
+    }
+    public int findPeakElement(int[] nums) {
+        return binarySearch(nums, 0, nums.length-1);
     }
 }
