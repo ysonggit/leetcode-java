6b6f6614f8904857b646e427913703f492004ed3
ysonggit <ysonggit@users.noreply.github.com>
2015-11-01 16:44:42 -0500

    Update P228SummaryRanges.java

--- P228SummaryRanges.java
+++ P228SummaryRanges.java
@@ -1,25 +1,16 @@
 public class Solution {
-    public String getRange(int start, int end){
-        if(start==end) return String.valueOf(start);
-        return String.valueOf(start).concat("->").concat(String.valueOf(end));
-    }
     public List<String> summaryRanges(int[] nums) {
         List<String> res = new ArrayList<String>();
-        int n = nums.length;
-        int start =0, end = 0;
+        int n = nums.length, start = 0, end=0;
+        if(n==0) return res;
         while(end<n){
-            if(end<n-1){
-                if(nums[end+1]-nums[end]==1){
-                    end++;
-                }else{
-                    res.add(getRange(nums[start], nums[end]));
-                    end++;
-                    start = end;
-                }
-            }else{
-                res.add(getRange(nums[start], nums[end]));
-                return res;
+            while(end<n-1 && nums[end+1]-nums[end]==1){
+                end++;
             }
+            if(start==end) res.add(String.valueOf(nums[start]));
+            else res.add(nums[start]+"->"+nums[end]);
+            end++;
+            start = end;
         }
         return res;
     }
