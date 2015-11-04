8a0ebf4f0e1d27ad16bfb1f7beb3478707422970
ysonggit <ysonggit@users.noreply.github.com>
2015-11-03 23:33:01 -0800

    Update P221MaximalSquare.java

--- P221MaximalSquare.java
+++ P221MaximalSquare.java
@@ -28,29 +28,17 @@ public class Solution1 {
         return max_area;
     }
 }
-public class Solution2 {
-    // DP
+
+public class Solution {
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
         if(n==0) return 0;
         int m = matrix[0].length;
-        int D[][] = new int[n][m];
+        int D[][] = new int[n+1][m+1];
         int max_len = 0;
-        for(int i=0; i<n; i++) {
-            if(matrix[i][0]=='1'){
-                D[i][0] = 1;
-                max_len = 1;
-            }
-        }
-        for(int j=0; j<m; j++){
-            if(matrix[0][j]=='1') {
-                D[0][j] = 1;
-                max_len=1;
-            }
-        } 
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-                if(matrix[i][j]=='0') D[i][j]=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(matrix[i-1][j-1]=='0') D[i][j]=0;
                 else{
                     D[i][j] = 1+Math.min(Math.min(D[i][j-1], D[i-1][j]), D[i-1][j-1]);
                 }
