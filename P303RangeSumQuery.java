4373e7e0093fb563d24107056af1958f2da6082e
ysonggit <ysonggit@users.noreply.github.com>
2015-11-10 01:41:51 -0500

    Update P303RangeSumQuery.java

--- P303RangeSumQuery.java
+++ P303RangeSumQuery.java
@@ -2,18 +2,14 @@ public class NumArray {
     int [] prefix_sum;
     public NumArray(int[] nums) {
         int n = nums.length;
-        if(n==0) return;
-        prefix_sum = new int[n];
-        prefix_sum[0] = nums[0];
-        for(int i=1; i<nums.length; i++){ // assume no overflow
-            prefix_sum [i] = prefix_sum[i-1] + nums[i];
+        prefix_sum = new int[n+1];
+        for(int i=0; i<nums.length; i++){ // assume no overflow
+            prefix_sum [i+1] = prefix_sum[i] + nums[i];
         }
     }
 
     public int sumRange(int i, int j) {
-        if(i>j || prefix_sum.length==0) return 0;
-        if(i==0) return prefix_sum[j];
-        return prefix_sum[j] - prefix_sum[i-1];
+        return prefix_sum[j+1] - prefix_sum[i];
     }
 }
 
