ef9e13162d1ab54b5d6af46fce5d1b9f1e17b08b
ysonggit <ysonggit@users.noreply.github.com>
2015-11-15 18:30:19 -0500

    Update P146LRUCache.java

--- P146LRUCache.java
+++ P146LRUCache.java
@@ -1,15 +1,15 @@
-import java.util.*;
+import java.util.LinkedHashMap;
+   /**
+    * HashMap makes absolutely no guarantees about the iteration order. 
+    *    It can (and will) even change completely when new elements are added.
+    * TreeMap will iterate according to the "natural ordering" of the keys according to their compareTo() method (or an externally supplied Comparator). 
+    *  Additionally, it implements the SortedMap interface, which contains methods that depend on this sort order.
+    * LinkedHashMap will iterate in the order in which the entries were put into the map
+    * 
+    */
 public class LRUCache {
-    /**
-     * HashMap makes absolutely no guarantees about the iteration order. 
-     *    It can (and will) even change completely when new elements are added.
-     * TreeMap will iterate according to the "natural ordering" of the keys according to their compareTo() method (or an externally supplied Comparator). 
-     *  Additionally, it implements the SortedMap interface, which contains methods that depend on this sort order.
-     * LinkedHashMap will iterate in the order in which the entries were put into the map
-     * 
-     */
+    LinkedHashMap<Integer, Integer> cache; // sorted by the insertion time
     int size;
-    LinkedHashMap<Integer, Integer> cache;
     public LRUCache(int capacity) {
         size = capacity;
         cache = new LinkedHashMap<Integer, Integer>();
@@ -17,19 +17,20 @@ public class LRUCache {
     
     public int get(int key) {
         if(!cache.containsKey(key)) return -1;
-        int val = cache.remove(key);
-        cache.put(key, val); // the feature of linkedhashmap
+        int val = cache.get(key);
+        cache.remove(key);
+        cache.put(key, val); //move the most visited item behind
         return val;
     }
     
     public void set(int key, int value) {
-        if(cache.containsKey(key)) {
+        if(cache.containsKey(key)){ // update key value
             cache.remove(key);
-            cache.put(key, value);
-            return;
-        }
-        if(cache.size()==size) { // find the least visited entry and remove
-            cache.remove(cache.entrySet().iterator().next().getKey());
+        }else{
+            if(size==cache.size()){ // remove the first entry
+                Object least_used_key = cache.entrySet().iterator().next().getKey();
+                cache.remove(least_used_key);
+            }
         }
         cache.put(key, value);
     }
