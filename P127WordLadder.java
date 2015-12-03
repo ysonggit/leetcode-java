8a73b6c5d175bdea49b6c85370de854e33acaa6e
ysonggit <ysonggit@users.noreply.github.com>
2015-12-02 23:46:01 -0800

    Update P127WordLadder.java

--- P127WordLadder.java
+++ P127WordLadder.java
@@ -1,46 +1,32 @@
 public class Solution {
-    public ArrayList<String> getOneEditWords(String word){
-        ArrayList<String> neighbors = new ArrayList<String>();
-        char wd[] = word.toCharArray();
-        for(int i=0; i<wd.length; i++){
-            char origin = wd[i];
-            for(char c = 'a'; c<='z'; c++){
-                if(wd[i] != c) { 
-                    wd[i] = c;
-                    neighbors.add(new String(wd));
-                    wd[i] = origin;
-                }
-            }
-        }
-        return neighbors;
-    }
-    public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
-        if(wordDict.size()==0) return 0;
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        Set<String> visited = new HashSet<String>();
         Queue<String> Q = new LinkedList<String>();
         Q.offer(beginWord);
-        HashSet<String> visited = new HashSet<String>();
         visited.add(beginWord);
-        int len = 1;
-        int curr_level = 1, next_level=0;
+        int len=1;
         while(!Q.isEmpty()){
-            String cur = Q.poll();
-            curr_level--;
-            for(String next : getOneEditWords(cur)){
-                if(next.equals(endWord)){ return len+1;}
-                if(wordDict.contains(next)) {
-                    if(!visited.contains(next)){
-                        Q.offer(next);
-                        visited.add(next);
-                        next_level++;
+            int num= Q.size();
+            for(int k=0; k<num; k++){
+                String cur = Q.poll();
+                char[] wd = cur.toCharArray();
+                for(int i=0; i<wd.length; i++){
+                    char orig = wd[i];
+                    for(char c = 'a'; c<='z'; c++){
+                        if(orig == c) continue;
+                        wd[i] = c;
+                        String nex = new String(wd);
+                        if(nex.equals(endWord)) return len+1;
+                        if(wordList.contains(nex) && !visited.contains(nex)){
+                            visited.add(nex);
+                            Q.offer(nex);
+                        }
                     }
+                    wd[i] = orig;
                 }
             }
-            if(curr_level==0){
-                curr_level = next_level;
-                next_level = 0;
-                len++;
-            }
+            len++;
         }
-        return 0;
+        return 0; // mistake: return len is wrong
     }
 }
