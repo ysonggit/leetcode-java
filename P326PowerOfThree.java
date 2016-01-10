520e5d3dd00682528a7506073a78f92a6bd9f009
ysonggit <ysonggit@users.noreply.github.com>
2016-01-09 22:35:57 -0800

    Create P326PowerOfThree.java

---
+++ P326PowerOfThree.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        while(n>1){
+            if(n%3!=0) return false;
+            n /= 3;
+        }
+        return true;
+    }
+}
