28bd5b177ac7f9201b0e37baa6d1bc687ffec34a
ysonggit <ysonggit@users.noreply.github.com>
2018-04-20 17:07:10 +0100

    Update P076MinimumWindowSubstring.java

--- P076MinimumWindowSubstring.java
+++ P076MinimumWindowSubstring.java
@@ -1,32 +1,24 @@
-public class Solution {
+class Solution {
     public String minWindow(String s, String t) {
-        int m = s.length(), n = t.length();
-        if(m<n) return "";
-        int dict_s[] = new int[256];
-        int dict_t[] = new int[256];
-        char s_arr[] = s.toCharArray();
-        char t_arr[] = t.toCharArray();
-        for(char c: t_arr) dict_t[(int)c]++;
-        int left =0, min_window_start = 0, min_len= m+1, count=0;
-        for(int right = 0; right<m; right++){
-            int cur = (int)s_arr[right];
-            if(dict_t[cur]==0) continue;
-            if(dict_t[cur] > dict_s[cur]) count++;
-            dict_s[cur]++;
-            while(count==n){ // move left
-                if(dict_s[(int) s_arr[left]]==0) left++;
-                else if(dict_s[(int) s_arr[left]] > dict_t[(int)s_arr[left]]){
-                    dict_s[(int) s_arr[left]]--;
-                    left++; // order matters!
-                }else{
-                    if(min_len > right-left+1){
-                        min_len = right-left+1;
-                        min_window_start = left;
-                    }
-                    break;// important
+        int [] need = new int[128];
+        int count = 0;
+        for(char c : t.toCharArray()) need[(int) c] ++;
+        int begin = 0, minLen = Integer.MAX_VALUE, head = 0;
+        for(int end = 0; end<s.length(); end++){
+            char c = s.charAt(end);
+            need[(int)c]--;
+            if(need[(int)c]>=0) count++; 
+            while(count == t.length()){ // find one substring
+                if(end-begin+1<minLen) {
+                    minLen = end - begin + 1;
+                    head = begin;
                 }
+                char h = s.charAt(begin);
+                need[(int)h] ++;
+                if(need[(int)h]>0) count--;
+                begin++;
             }
         }
-        return (min_len>m) ? "" : s.substring(min_window_start, min_window_start+min_len);
+        return minLen > s.length() ? "" : s.substring(head, minLen+head);
     }
 }
